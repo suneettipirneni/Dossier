51002374427f91fadd0ce38c5aa8ef4d2b8e773b
suneettipirneni <suneettipirneni@icloud.com>
2021-09-05 17:07:33 -0400

    add bulma

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import './bulma-override.scss';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
+        <button className="button is-primary">Hello World</button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
